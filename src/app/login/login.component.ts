eb8afb2e53318967a1b08c55a511abe3760da8b2
Bartlomiej Borzucki <borzucki@gmail.com>
2017-12-30 00:22:47 +0100

    Add validation to the login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,47 +3,60 @@ import {UserService} from '../services/userService'
 import {Response} from '@angular/http';
 import {UserLogin} from '../models/user';
 import {Router} from '@angular/router';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, FormControl, AbstractControl} from '@angular/forms';
 
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+    loginForm: FormGroup;
 
-  constructor(fb: FormBuilder, protected router: Router, private userService: UserService) {
-    this.loginForm = fb.group({
-      'email': ['', [Validators.required, Validators.email]],
-      'password': ['', Validators.required]
-    });
-  }
+    constructor(protected router: Router, private userService: UserService) {
+        this.loginForm = new FormGroup({
+            'email': new FormControl('', [Validators.required, Validators.email]),
+            'password': new FormControl('', Validators.required)
+        });
+        this.loginForm.controls['email'].valueChanges.subscribe(() => {
+            this.resetValidationError();
+        });
 
-  ngOnInit() {
-  }
+        this.loginForm.controls['password'].valueChanges.subscribe(() => {
+            this.resetValidationError();
+        });
+    }
 
-  getErrorMessage() {
-    return this.loginForm.controls['email'].hasError('required') ? 'You must enter a value' :
-      this.loginForm.controls['email'].hasError('email') ? 'Not a valid email' :
-        '';
-  }
+    private resetValidationError() {
+        this.loginForm.controls['email'].setErrors(null);
+        this.loginForm.controls['password'].setErrors(null);
+    }
 
-  onSubmit(values: any) {
-    console.log(values);
-    this.userService.login(values).subscribe(
-      (response: Response) => { // on sucesss
-        console.log(response);
-        this.router.navigate(['home']);
-      },
-      (err: any) => { // on error
-        console.log(err);
-      },
-      () => { // on completion
+    ngOnInit() {
+    }
 
-      }
-    );
-  }
+    getErrorMessage() {
+        return this.loginForm.controls['email'].hasError('required') ? 'You must enter a value' :
+            this.loginForm.controls['email'].hasError('email') ? 'Not a valid email' :
+                '';
+    }
+
+    onSubmit(values: any) {
+        console.log(values);
+        this.userService.login(values).subscribe(
+            (response: Response) => { // on sucesss
+                console.log(response);
+                this.router.navigate(['home']);
+            },
+            (err: any) => { // on error
+                this.loginForm.controls['email'].setErrors({'incorrectLoginPassword': true});
+                this.loginForm.controls['password'].setErrors({'incorrectLoginPassword': true});
+            },
+            () => { // on completion
+
+            }
+        );
+    }
 
 }
