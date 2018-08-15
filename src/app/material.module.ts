ea889aa3d83c0f82bdd32b73d9f3f420d7f63b84
Bartlomiej Borzucki <borzucki@gmail.com>
2018-08-15 13:37:23 +0200

    Fix error TRIDENT is undefined

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,8 +12,9 @@ import {
     MatProgressBarModule,
     MatProgressSpinnerModule, MatRadioModule, MatSelectModule, MatSidenavModule, MatSlideToggleModule,
     MatSnackBarModule, MatToolbarModule,
-    MatTooltipModule, MatExpansionModule
+    MatTooltipModule, MatExpansionModule, MAT_DATE_LOCALE
 } from '@angular/material';
+import {Platform, PlatformModule} from '@angular/cdk/platform';
 import {NgModule} from '@angular/core';
 import {MyDateAdapter} from './shared/data-adapter';
 
@@ -21,14 +22,14 @@ import {MyDateAdapter} from './shared/data-adapter';
     imports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatInputModule, MatRadioModule, MatSelectModule,
         MatSlideToggleModule, MatMenuModule, MatSidenavModule, MatToolbarModule, MatListModule, MatCardModule,
         MatButtonToggleModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule,
-        MatTooltipModule, MatSnackBarModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule],
+        MatTooltipModule, MatSnackBarModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, PlatformModule],
     providers: [
-        {provide: DateAdapter, useClass: MyDateAdapter}
+        {provide: DateAdapter, useClass: MyDateAdapter, deps: [MAT_DATE_LOCALE, Platform]}
     ],
     exports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatInputModule, MatRadioModule, MatSelectModule,
         MatSlideToggleModule, MatMenuModule, MatSidenavModule, MatToolbarModule, MatListModule, MatCardModule,
         MatButtonToggleModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule,
-        MatTooltipModule, MatSnackBarModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule],
+        MatTooltipModule, MatSnackBarModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, PlatformModule],
     declarations: []
 })
 export class TickistMaterialModule {
