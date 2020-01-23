49bbaade9c571f5aa979e2f3947ce22671df55f6
Bartlomiej Borzucki <borzucki@gmail.com>
2020-01-23 23:43:34 +0100

    Remove disable-dev-shm-usage

--- apps/tickist-web-e2e/src/plugins/index.js
+++ apps/tickist-web-e2e/src/plugins/index.js
@@ -19,14 +19,14 @@ const cypressFirebasePlugin = require('cypress-firebase').plugin;
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
-    on('before:browser:launch', (browser = {}, args) => {
-        if (browser.name === 'chrome') {
-            args.push('--disable-dev-shm-usage');
-            return args
-        }
-
-        return args
-    });
+    // on('before:browser:launch', (browser = {}, args) => {
+    //     if (browser.name === 'chrome') {
+    //         args.push('--disable-dev-shm-usage');
+    //         return args
+    //     }
+    //
+    //     return args
+    // });
 
     on('task', {
         failed: require('cypress-failed-log/src/failed')(),
