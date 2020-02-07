8ec9983519339c282a9127015c44469ec558f6de
Bartlomiej Borzucki <borzucki@gmail.com>
2020-02-08 00:14:20 +0100

    Bump AngularFire

--- apps/tickist-web/src/app/core/services/auth.service.ts
+++ apps/tickist-web/src/app/core/services/auth.service.ts
@@ -37,22 +37,15 @@ export class AuthService {
         return this.fireAuth.auth.signOut();
     }
 
-    checkEmail(email) {
-        // return this.http.post(`${environment.apiUrl}/check_email/`, {'email': email});
-
-    }
-
     save({uid, username, email}) {
         const user = new User(<any> {id: uid, username: username, email: email});
         this.usersCollection.doc(uid).set(JSON.parse(JSON.stringify(user)))
             .then(() => {
-                this.store.dispatch(new FetchedLoginUser({uid: uid}))
+                this.store.dispatch(new FetchedLoginUser({uid: uid}));
                 this.router.navigateByUrl('/');
             })
             .catch((err) => console.log(err));
     }
-
-
 }
 
 
