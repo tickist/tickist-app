46291940ca9fc413597172c35c1a7a4134c37208
Bartlomiej Borzucki <borzucki@gmail.com>
2020-06-13 23:44:32 +0200

    Fix - user avatar url

--- apps/tickist-web/src/app/app.module.ts
+++ apps/tickist-web/src/app/app.module.ts
@@ -59,6 +59,7 @@ import {TickistLeftPanelModule} from './modules/left-panel/left-panel.module';
             maxAge: 25, // Retains last 25 states
             logOnly: environment.production, // Restrict extension to log-only mode
         }),
+        environment.production ? StoreDevtoolsModule.instrument({maxAge: 50}) : [],
         SortablejsModule.forRoot({
             animation: 150
         }),
