c62a66bbc86716be5798cfb62d459ac20f3b1274
Bartlomiej Borzucki <borzucki@gmail.com>
2020-10-24 23:25:50 +0200

    Bump packages

--- apps/tickist-web/jest.config.js
+++ apps/tickist-web/jest.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-    name: 'tickist-web',
-    preset: '../../jest.config.js',
-    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-    globals: {
-        'ts-jest': {
-            tsConfig: '<rootDir>/tsconfig.spec.json',
-            stringifyContentPathRegex: '\\.(html|svg)$',
-            astTransformers: [
-                'jest-preset-angular/build/InlineFilesTransformer',
-                'jest-preset-angular/build/StripStylesTransformer',
-            ],
-        },
+  preset: "../../jest.preset.js",
+  setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.(html|svg)$",
+      astTransformers: {
+        before: [
+          "jest-preset-angular/build/InlineFilesTransformer",
+          "jest-preset-angular/build/StripStylesTransformer",
+        ],
+      },
     },
-    coverageDirectory: '../../coverage/apps/investment',
-    snapshotSerializers: [
-        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-        'jest-preset-angular/build/AngularSnapshotSerializer.js',
-        'jest-preset-angular/build/HTMLCommentSerializer.js',
-    ],
+  },
+  coverageDirectory: "../../coverage/apps/investment",
+  snapshotSerializers: [
+    "jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js",
+    "jest-preset-angular/build/AngularSnapshotSerializer.js",
+    "jest-preset-angular/build/HTMLCommentSerializer.js",
+  ],
+  displayName: "tickist-web",
 };
