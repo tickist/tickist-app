54472d1a80c784657f9b70d47bb6175bca53d729
Bartlomiej Borzucki <borzucki@gmail.com>
2023-01-04 00:17:20 +0100

    Bump firebase functions

--- apps/tickist-functions/src/main.ts
+++ apps/tickist-functions/src/main.ts
@@ -1,15 +1,8 @@
-import * as functions from 'firebase-functions';
-
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export {onCreateUser} from './app/users/on-create-user';
-export {onUpdateUser} from './app/users/on-update-user';
-export {onUpdateProject, createUpdateProjectNotifications} from './app/projects/on-update-project';
-export {onUpdateTag} from './app/tags/on-update-tag';
-export {onDeleteTag} from './app/tags/on-delete-tag';
-export {createTaskNotification} from './app/tasks/on-create-task';
-export {onUpdateTask, createUpdateTaskNotifications} from './app/tasks/on-update-task';
-export {onCreateNotification} from './app/notifications/on-create-notification';
-
+export { onCreateUser } from "./app/users/on-create-user";
+export { onUpdateUser } from "./app/users/on-update-user";
+export { onUpdateProject, createUpdateProjectNotifications } from "./app/projects/on-update-project";
+export { onUpdateTag } from "./app/tags/on-update-tag";
+export { onDeleteTag } from "./app/tags/on-delete-tag";
+export { createTaskNotification } from "./app/tasks/on-create-task";
+export { onUpdateTask, createUpdateTaskNotifications } from "./app/tasks/on-update-task";
+export { onCreateNotification } from "./app/notifications/on-create-notification";
