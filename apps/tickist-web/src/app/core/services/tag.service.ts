d024814c01ea851bf0e291f55fbbd9e6711765f2
Bartlomiej Borzucki <borzucki@gmail.com>
2023-08-13 17:43:13 +0200

    Fix Tags

--- apps/tickist-web/src/app/core/services/tag.service.ts
+++ apps/tickist-web/src/app/core/services/tag.service.ts
@@ -1,14 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Tag } from "@data/tags/models/tags";
-import {
-    addDoc,
-    collection,
-    deleteDoc,
-    doc,
-    Firestore,
-    setDoc,
-    updateDoc,
-} from "@angular/fire/firestore";
+import { addDoc, collection, deleteDoc, doc, Firestore, setDoc, updateDoc } from "@angular/fire/firestore";
 
 const tagsCollectionName = "tags";
 
@@ -19,23 +11,29 @@ export class TagService {
     constructor(private firestore: Firestore) {}
 
     async createTag(tag: Tag) {
-        console.log("HERE HERE HERE HERE HERE HERE");
-        const docRef = doc(collection(this.firestore, tagsCollectionName));
-        // const newTagRef = this.db.collection(tagsCollectionName).ref.doc();
-        // return newTagRef.set({ ...tag, id: newTagRef.id });
-        // const docRef = doc(this.firestore, `${tagsCollectionName}`);
-        // await setDoc(docRef, { ...tag, id: docRef.id });
-        await setDoc(docRef, {
-            id: docRef.id,
-            ...tag,
-        });
+        try {
+            console.log("HERE HERE HERE HERE HERE HERE");
+            const docRef = doc(collection(this.firestore, tagsCollectionName));
+            await setDoc(docRef, {
+                id: docRef.id,
+                ...tag,
+            });
+        } catch (e) {
+            console.error({ e });
+        }
     }
 
-    createTagDuringEditingTask(tag: Tag) {
-        console.log("HERE HERE HERE HERE HERE HERE");
-        const docRef = doc(this.firestore, `${tagsCollectionName}`);
-        // const newTagRef = this.db.collection(tagsCollectionName).ref.doc();
-        return this.createTag(tag).then(() => docRef.id);
+    async createTagDuringEditingTask(tag: Tag) {
+        try {
+            const docRef = doc(collection(this.firestore, tagsCollectionName));
+            await setDoc(docRef, {
+                id: docRef.id,
+                ...tag,
+            });
+            return docRef.id;
+        } catch (e) {
+            console.error({ e });
+        }
     }
 
     async updateTag(tag: Tag) {
--- apps/tickist-web/src/app/material.module.ts
+++ apps/tickist-web/src/app/material.module.ts
@@ -1,50 +1,98 @@
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import {  MatCheckboxModule } from '@angular/material/checkbox';
-import {  MatChipsModule } from '@angular/material/chips';
-import { DateAdapter, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import {  MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {  MatRadioModule } from '@angular/material/radio';
-import {  MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {  MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTreeModule } from '@angular/material/tree';
-import {MatTabsModule} from '@angular/material/tabs';
-import {Platform, PlatformModule} from '@angular/cdk/platform';
-import {NgModule} from '@angular/core';
-import {MyDateAdapter} from './shared/data-adapter';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {MatBadgeModule} from '@angular/material/badge';
-
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from "@angular/material/chips";
+import { DateAdapter, MatNativeDateModule, MAT_DATE_LOCALE } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatTabsModule } from "@angular/material/tabs";
+import { Platform, PlatformModule } from "@angular/cdk/platform";
+import { NgModule } from "@angular/core";
+import { MyDateAdapter } from "./shared/data-adapter";
+import { ScrollingModule } from "@angular/cdk/scrolling";
+import { MatBadgeModule } from "@angular/material/badge";
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatInputModule, MatRadioModule, MatSelectModule,
-        MatSlideToggleModule, MatMenuModule, MatSidenavModule, MatToolbarModule, MatListModule, MatCardModule,
-        MatButtonToggleModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule,
-        MatTooltipModule, MatSnackBarModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, PlatformModule, MatTabsModule,
-        ScrollingModule, MatInputModule, MatTreeModule, MatBadgeModule],
-    providers: [
-        {provide: DateAdapter, useClass: MyDateAdapter, deps: [MAT_DATE_LOCALE, Platform]}
+    imports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatAutocompleteModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatCardModule,
+        MatButtonToggleModule,
+        MatChipsModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatProgressBarModule,
+        MatDialogModule,
+        MatTooltipModule,
+        MatSnackBarModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatExpansionModule,
+        PlatformModule,
+        MatTabsModule,
+        ScrollingModule,
+        MatInputModule,
+        MatTreeModule,
+        MatBadgeModule,
     ],
-    exports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatInputModule, MatRadioModule, MatSelectModule,
-        MatSlideToggleModule, MatMenuModule, MatSidenavModule, MatToolbarModule, MatListModule, MatCardModule,
-        MatButtonToggleModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule,
-        MatTooltipModule, MatSnackBarModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, PlatformModule, MatTabsModule,
-        ScrollingModule, MatInputModule, MatTreeModule, MatBadgeModule],
-    declarations: []
+    providers: [{ provide: DateAdapter, useClass: MyDateAdapter, deps: [MAT_DATE_LOCALE, Platform] }],
+    exports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatAutocompleteModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatCardModule,
+        MatButtonToggleModule,
+        MatChipsModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatProgressBarModule,
+        MatDialogModule,
+        MatTooltipModule,
+        MatSnackBarModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatExpansionModule,
+        PlatformModule,
+        MatTabsModule,
+        ScrollingModule,
+        MatInputModule,
+        MatTreeModule,
+        MatBadgeModule,
+    ],
+    declarations: [],
 })
-export class TickistMaterialModule {
-}
+export class TickistMaterialModule {}
