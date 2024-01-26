48fae9e3d757d87e52be0c28a3b9804853b69f67
Bartlomiej Borzucki <borzucki@gmail.com>
2024-01-27 00:31:53 +0100

    Fix importScript issue

--- apps/tickist-web/src/combined-sw.js
+++ apps/tickist-web/src/combined-sw.js
@@ -1,2 +1,4 @@
-importScripts('/ngsw-worker.js');
-importScripts('/firebase-messaging-sw.js');
+// eslint-disable-next-line no-undef
+importScripts("/ngsw-worker.js");
+// eslint-disable-next-line no-undef
+importScripts("/firebase-messaging-sw.js");
